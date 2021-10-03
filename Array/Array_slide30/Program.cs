835d7a2f82eb3590d9c8adccaa17718dc6277988
Nguyễn Thị Ngọc Hiền <66470309+neihcogn49520@users.noreply.github.com>
2021-10-03 22:27:31 +0700

    Array C#

---
+++ Array/Array_slide30/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array_slide30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mang trong C#");
+            Console.WriteLine("------------------------\n");
+            int[] arr = new int[10]; //arr la mang gom 10 phan tu so nguyen
+            //khoi tao cac phan tu cua mang
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = i + 100;
+            }
+            //hien thi gia tri cac phan tu cua mang
+            for (int j = 0; j < 10; j++)
+            {
+                Console.WriteLine("Phan tu thu [{0}] = {1}", j, arr[j]);
+            }
+        }
+    }
+}
---
+++ Array/Array_slide31/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Array_slide31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mang trong C#");
+            Console.WriteLine("------------------------\n");
+            int[] arr = new int[10];
+
+            //khoi tao cac phan tu cua mang
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = i + 100;
+            }
+
+            //hien thi gia tri cac phan tu cua mang
+            foreach (int j in arr)
+            {
+                int i = j - 100;
+                Console.WriteLine("Phan tu thu [{0}] = {1}", i, j);
+            }
+        }
+    }
+}
